043b21678eb7c87083b6f535e793336eec5d1a67
Mr.solo <492663515@qq.com>
2018-01-08 13:49:02 +0800

    11

--- Application/Mobile/Controller/RoomsController.class.php
+++ Application/Mobile/Controller/RoomsController.class.php
@@ -3,6 +3,21 @@ namespace Mobile\Controller;
 use Think\Controller;
 use Think\D;
 class RoomsController extends CommonController{
+	public $model = 'House';
+	public function _map(&$data)
+	{
+		if (I('cate')) {
+			$map['H.category'] = I('cate');
+			//设定房间分类进来的情况
+		}
+		$data = [
+			'alias' => 'H',
+			'where' => $map,
+			'join'  => 'LEFT JOIN __FILES__ F ON F.id = H.pic',
+			'field' => "H.id,H.name,H.word,CONCAT('/Uploads',F.savepath,F.savename) `icon`"
+		];
+
+	}
 	/**
 	 * [index 客房]
 	 * @Author   ヽ(•ω•。)ノ   Mr.Solo
@@ -12,7 +27,18 @@ class RoomsController extends CommonController{
 	 */
 	public function index()
 	{
-		$this->display();
+		// echo parent::index('sql');die;
+		if (IS_AJAX) {
+			parent::index();
+		}else{
+			//获取分类信息
+			$cates = D::get('HouseCate',[
+				'status' => '1',
+				'type' => 'h',
+			]);
+			$this->assign('cates',$cates);
+			$this->display();
+		}
 	}
 	/**
 	 * [edit 客房详情页]
@@ -21,8 +47,13 @@ class RoomsController extends CommonController{
 	 * @Function []
 	 * @return   [type]     [description]
 	 */
-	public function edit()
+	public function edit($id)
 	{
+		$house = D::find('House',$id); //获取房屋信息
+		$bannerMap['id'] = ['in',array_filter(explode(',', $house['imgs']))]; //banner ids
+		$Banners = D::get('Files',$bannerMap); //获取banner
+		$this->assign('banners',$Banners);
+		$this->assign('db',$house);
 		$this->display();
 	}
 }
